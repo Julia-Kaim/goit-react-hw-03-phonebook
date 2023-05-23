ed551d460f258451190378dd2b6669559f05e605
Julia-Kaim <julia.kaim08@gmail.com>
2023-05-23 10:26:09 +0200

    feat: localStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -59,6 +59,21 @@ class App extends Component {
     }));
   };
 
+  componentSetStorage() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   render() {
     const { contacts, filter } = this.state;
     const visibleContacts = this.getVisibleContacts();
